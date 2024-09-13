f146e95e12b894f324c0ab69cdfc8ab143957281
austi <ajs396s@login.missouristate.edu>
2024-09-12 19:40:34 -0500

    Final Commit

--- app/src/main/java/com/example/trafficlight/MainActivity.kt
+++ app/src/main/java/com/example/trafficlight/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.trafficlight
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,14 +17,97 @@ class MainActivity : AppCompatActivity() {
     private var isWait = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Sets button color
+        binding.trafficLightButton.apply {
+            setBackgroundColor(getColor(R.color.red))
+            text = "STOP"
+        }
+
+        // Set up button listener to cycle the button color and text
+        binding.trafficLightButton.setOnClickListener {
+            if(isStop) {
+                // set Green
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.green))
+                    text = "GO"
+                }
+                // Reset cycle
+                isStop = false
+                isGo = true
+                isWait = false
+            } else if (isGo) {
+                // set Yellow
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.yellow))
+                    text = "WAIT"
+                }
+                // Next cycle
+                isStop = false
+                isGo = false
+                isWait = true
+            } else if (isWait) {
+                // set red
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.red))
+                    text = "STOP"
+                }
+                // New cycle
+                isStop = true
+                isGo = false
+                isWait = false
+            }
+        }
+        // Cycle the traffic light images
+        binding.trafficLightButton.setOnClickListener {
+            if (isStop){
+                binding.redLight.visibility = View.INVISIBLE
+                binding.yellowLight.visibility = View.INVISIBLE
+                binding.greenLight.visibility = View.VISIBLE
+
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.green))
+                    text = "GO"
+                }
+                isStop = false
+                isGo = true
+                isWait = false
+            } else if (isGo){
+                binding.redLight.visibility = View.INVISIBLE
+                binding.yellowLight.visibility = View.VISIBLE
+                binding.greenLight.visibility = View.INVISIBLE
+
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.yellow))
+                    text = "WAIT"
+                }
+                isStop = false
+                isGo = false
+                isWait = true
+            } else if (isWait) {
+                binding.redLight.visibility = View.VISIBLE
+                binding.yellowLight.visibility = View.INVISIBLE
+                binding.greenLight.visibility = View.INVISIBLE
+
+                binding.trafficLightButton.apply {
+                    setBackgroundColor(getColor(R.color.red))
+                    text = "STOP"
+                }
+                isStop = true
+                isGo = false
+                isWait = false
+            }
+        }
+
+
         }
     }
-}
+
+
+
+
+
